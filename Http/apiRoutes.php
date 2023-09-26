b72304b4a2e0df238c7e791e123998a6275bff07
Omar Chunza <odchunzag.une@gmail.com>
2023-09-26 11:40:09 -0500

    Actualizacion de modulo Ibinnacle a laravel 10.

--- Http/apiRoutes.php
+++ Http/apiRoutes.php
@@ -2,13 +2,12 @@
 
 use Illuminate\Routing\Router;
 
-$router->group(['prefix' =>'/ibinnacle/v1'], function (Router $router) {
+Route::prefix('/ibinnacle/v1')->group(function (Router $router) {
     $router->apiCrud([
-      'module' => 'ibinnacle',
-      'prefix' => 'binnacles',
-      'controller' => 'BinnacleApiController',
-      'middleware' => ['create' => [], 'index' => [], 'show' => [], 'update' => [], 'delete' => [], 'restore' => []]
+        'module' => 'ibinnacle',
+        'prefix' => 'binnacles',
+        'controller' => 'BinnacleApiController',
+        'middleware' => ['create' => [], 'index' => [], 'show' => [], 'update' => [], 'delete' => [], 'restore' => []],
     ]);
-// append
-
+    // append
 });
